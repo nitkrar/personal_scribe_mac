76d7526b8c633aca3a28335516d8da64ef9e7c29
Nitin Kumar <coolnits88@gmail.com>
2026-04-17 21:44:40 +0100

    plan-01 step 3.8: scaffold empty composition directory

---
+++ Tests/SeshatAppKitTests/CompositionDirectoryLayoutTests.swift
@@ -0,0 +1,8 @@
+import XCTest
+@testable import SeshatAppKit
+
+final class CompositionDirectoryLayoutTests: XCTestCase {
+    func testCompositionDirectoryLayoutWitness() {
+        XCTAssertTrue(true)
+    }
+}
