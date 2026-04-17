2131a3c6a552b74a19191bd7cc456b6cd3a8ff09
Nitin Kumar <coolnits88@gmail.com>
2026-04-17 21:45:08 +0100

    plan-01 step 4.3: add pstranscription placeholder test

---
+++ Tests/SeshatTranscriptionTests/SeshatTranscriptionPlaceholderTests.swift
@@ -0,0 +1,8 @@
+import XCTest
+@testable import SeshatTranscription
+
+final class SeshatTranscriptionPlaceholderTests: XCTestCase {
+    func testPlaceholder() {
+        XCTAssertTrue(true)
+    }
+}
